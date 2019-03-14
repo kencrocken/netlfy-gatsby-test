af3abdf235ef373213e165b73dc74761d4ca97de
Ken Crocken <kcrocken@gmail.com>
2019-03-14 19:50:12 -0400

    adjust typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,16 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography'
+import FunstonTheme from 'typography-theme-funston'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
+
+FunstonTheme.baseFontSize = '18px'
+FunstonTheme.baseLineHeight = 1.618
+FunstonTheme.scaleRatio = 1.618
+FunstonTheme.overrideThemeStyles = () => ({
+  body: {
+      letterSpacing: ".05em",
   }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+})
+const typography = new Typography(FunstonTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
